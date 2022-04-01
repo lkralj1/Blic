2aec9ad7c813f20c6d0aab5b4b4b73674b917f64
Luka <luka.kralj123@gmail.com>
2022-04-01 10:23:47 +0200

    blic

---
+++ src/views/About.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="commit">
+    <h1>Detalji knjige</h1>
+    <ul>
+      <li>Ime autora: {{ commit.commit.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  props: {
+    url: String,
+  },
+
+  data() {
+    return {
+      commit: [],
+    };
+  },
+
+  created: async function () {
+    let get_request = await fetch(
+      "https://www.anapioficeandfire.com/api/books" + this.url
+    );
+    let response = await get_request.json();
+    this.commit = response;
+    console.log(get_request);
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <ol>
+      <li v-for="commit in commits" :key="commit.url">
+        <!-- <router-link :to="'/commit_details/' + commit.sha">{{ commit.sha }}</router-link> -->
+        <router-link :to="{ name: 'About', params: { url: commit.url } }">{{
+          commit.url
+        }}</router-link>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      commits: [],
+    };
+  },
+
+  mounted: async function () {
+    let get_request = await fetch(
+      "https://www.anapioficeandfire.com/api/books"
+    );
+    let response = await get_request.json();
+    this.commits = response;
+  },
+};
+</script>
